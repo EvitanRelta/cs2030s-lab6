cccc614d34217b8d2834478699b9ac496eb291ca
Shaun Tan <shauntanzongzhi@gmail.com>
2022-03-17 21:50:24 +0800

    Replace 'run_all_tests.sh' with 'partial_test.sh'

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,10 +1,14 @@
 javac -Xlint:rawtypes,unchecked ./cs2030s/fp/*.java
-javac -Xlint:rawtypes,unchecked *.java
-java Test1 | grep failed;
-java Test2 | grep failed;
-java Test3 | grep failed;
-java Test4 | grep failed;
-java Lab5
-printf "A\nA-\nA+\nA\nC\nNo such entry\nNo such entry\nNo such entry\n"
+javac -Xlint:rawtypes,unchecked Test1.java; java Test1
+javac -Xlint:rawtypes,unchecked Test2.java; java Test2
+javac -Xlint:rawtypes,unchecked Test3.java; java Test3
+javac -Xlint:rawtypes,unchecked Test4.java; java Test4
+javac -Xlint:rawtypes,unchecked Test5.java; java Test5
+
+# Check style
 java -jar ~cs2030s/bin/checkstyle.jar -c ~cs2030s/bin/cs2030_checks.xml cs2030s/fp/*.java;
 java -jar ~cs2030s/bin/checkstyle.jar -c ~cs2030s/bin/cs2030_checks.xml *.java;
+
+# Test generating 'javadoc'
+javadoc -quiet -private -d docs cs2030s/fp/Lazy.java
+javadoc -quiet -private -d docs LazyList.java
