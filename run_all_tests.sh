bb4ff99a01d4da0e50eb5ee860dcd63fcfbf5f2d
Shaun Tan <shauntanzongzhi@gmail.com>
2022-03-19 12:25:45 +0800

    Add 'LazyList' compilation to 'run_all_tests.sh'
    Passes all Tests + checkstyle + javadoc

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -3,6 +3,8 @@ javac -Xlint:rawtypes,unchecked Test1.java; java Test1
 javac -Xlint:rawtypes,unchecked Test2.java; java Test2
 javac -Xlint:rawtypes,unchecked Test3.java; java Test3
 javac -Xlint:rawtypes,unchecked Test4.java; java Test4
+
+javac -Xlint:rawtypes,unchecked LazyList.java
 javac -Xlint:rawtypes,unchecked Test5.java; java Test5
 
 # Check style
